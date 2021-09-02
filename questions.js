f0928521c48146c4c78ae7f64436042261b4b084
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-09-02 12:40:04 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"_____ my friend, Veronica. She is _____ artist.",
+        options:["This is / an", "She is / a", "That is / —", "These are / the"],
+        answer: 0
+    },
+    {
+        q:"He lives near _____ Lake Michigan.",
+        options:["a", "-", "an", "the"],
+        answer: 1
+    },
+    {
+        q:"They have _____ car and _____ in the garage.",
+        options:["an / they are", "— / that is", "a / it is", "the / this is"],
+        answer: 2
+    },
+    {
+        q:"A: _____ a new book? - B: Yes, _____ .",
+        options:["Is that / that is", "Is it / this is", "Are those / they are", "Is this / it is"],
+        answer: 3
+    },
+    {
+        q:"_____ Pyramids are in _____ Egypt.",
+        options:["The / —", "An / the", "— / the", "Some / an"],
+        answer: 0
+    },
+    {
+        q:"A: How much are _____ roses? - B: _____ £10.",
+        options:["this / It is", "these / They are", "that / Those are", "those / It is"],
+        answer: 1
+    },
+    {
+        q:"A: Is Mary's husband here? - B: No, _____. He is in _____ United Kingdom.",
+        options:["this isn't / the", "it isn't / —", "he isn't / the", "she isn't / a"],
+        answer: 2
+    },
+    {
+        q:"_____ is _____ expensive ring.",
+        options:["These / the", "This / a", "It / —", "That / an"],
+        answer: 3
+    },
+    {
+        q:"A: Where is_____ Dublin? - B: It is in _____ Ireland.",
+        options:["— / —", "— / the", "the / —", "a / an"],
+        answer: 0
+    },
+    {
+        q:"There is _____ museum in the city centre. _____ museum is full of antique jewellery.",
+        options:["— / Some", "a / The", "the / —", "some / A"],
+        answer: 1
+    }
+
+
+
+]
